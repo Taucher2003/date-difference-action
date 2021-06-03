3f11f7b1ac0c81b33c2646cb172a0d2d0bb1437e
Niklas van Schrick <mc.taucher2003@gmail.com>
2021-06-03 19:12:35 +0200

    fix regex

--- main.py
+++ main.py
@@ -58,6 +58,10 @@
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 #
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#
 import re
 import os
 import base64
@@ -67,7 +71,7 @@ from dateutil.relativedelta import relativedelta
 from github import Github, GithubException
 
 REGEX = r'(?P<full>(<!--timespan:start' \
-        r'\((?P<format>[a-zA-Z.\/\\\\]+)\)' \
+        r'\((?P<format>[%a-zA-Z.\/\\\\]+)\)' \
         r'((\((?P<date>(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.][0-9]{4})\))' \
         r'|(\(env:(?P<id>[0-9]+)\)))' \
         r'-->[0-9]*<!--timespan:end-->))'
