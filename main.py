2975f74813da9de8585739d431a2602128ae08fa
Niklas van Schrick <mc.taucher2003@gmail.com>
2021-06-03 19:06:21 +0200

    debug

--- main.py
+++ main.py
@@ -50,6 +50,10 @@
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 #
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#
 import re
 import os
 import base64
@@ -73,8 +77,8 @@ def generate_readme_content(old_content: str) -> str:
     content = old_content
     matches = re.findall(REGEX, old_content)
     for match in matches:
-        print(match)
-        print(match[0])
+        print("match: " + match)
+        print("match[0]: " + match[0])
         re.sub(match[0], replace(match[0]), content)
 
     return content
@@ -149,9 +153,9 @@ if __name__ == '__main__':
         sys.exit(1)
     contents = repo.get_readme()
     readme_content = decode_readme(contents.content)
-    print(readme_content)
+    print("readme_content: "+readme_content)
     replaced_content = generate_readme_content(readme_content)
-    print(replaced_content)
+    print("replaced_content: "+replaced_content)
     if replaced_content != readme_content:
         repo.update_file(path=contents.path, message=commit_message,
                          content=replaced_content, sha=contents.sha)
