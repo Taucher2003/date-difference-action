a61443f6a7a85b9241b4574609130e3378785f4c
Niklas van Schrick <mc.taucher2003@gmail.com>
2021-06-03 20:39:37 +0200

    implement months for days

--- main.py
+++ main.py
@@ -150,6 +150,10 @@
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 #
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#
 import re
 import os
 import base64
@@ -225,7 +229,7 @@ def calculate_days(start: datetime, end: datetime, years: bool, months: bool) ->
 
     if months:
         """TODO"""
-        result = result
+        result = relativedelta(end, start).days
 
     return result
 
