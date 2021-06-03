98bfa4bc7d7901df4967eeb8d1ccead0eac8e3a4
Niklas van Schrick <mc.taucher2003@gmail.com>
2021-06-03 20:49:10 +0200

    fix regex

--- main.py
+++ main.py
@@ -158,6 +158,10 @@
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 #
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#
 import re
 import os
 import base64
@@ -170,7 +174,7 @@ REGEX = r'(?P<full>(<!--timespan:start' \
         r'\((?P<format>[%a-zA-Z-., \/\\\\]+)\)' \
         r'((\((?P<date>[0-9]{4}[- /.](0[1-9]|1[0-2])[- /.](0[1-9]|[12][0-9]|3[01]))\))' \
         r'|(\(env:(?P<id>[0-9]+)\)))' \
-        r'-->(?P<replace>[\S]*)<!--timespan:end-->))'
+        r'-->(?P<replace>[\S ]*)<!--timespan:end-->))'
 
 repository = os.getenv('INPUT_REPOSITORY')
 github_token = os.getenv('INPUT_GITHUB_TOKEN')
